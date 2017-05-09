4e81b704ca402b03167a4eeb94dba47a4c7c1d37
supercodingfan <supercodingfan@gmail.com>
2017-05-08 21:16:59 -0500

    Fix missing curly

--- models/product.js
+++ models/product.js
@@ -1,54 +1,53 @@
 module.exports = function(sequelize, DataTypes) {
-  var Product = sequelize.define("product", {
-      	id: {
-        	type: DataTypes.INTEGER,
+    var Product = sequelize.define("product",{
+        id: {
+            type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true
         },
         name: {
-        	type: DataTypes.STRING,
-        	allowNull: false,
-        	validate: {}
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {}
         },
         price: {
-        	type: DataTypes.INTEGER,
-        	allowNull: false,
-        	validate: {}
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {}
         },
         img: {
-        	type: DataTypes.STRING,
-        	allowNull: false,
-        	validate: {}
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {}
         },
         desc: {
-        	type: DataTypes.TEXT,
-        	allowNull: false,
-        	validate: {}
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {}
         },
         category: {
-        	type: DataTypes.STRING, 
-        	allowNull: false,
-        	validate: {}
-        }, 
-        quantity: {
-        	type: DataTypes.INTEGER, 
-        	allowNull: false,
-        	validate: {}
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {}
         },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {}
+        }
+    },
         {
-        classMethods: {
-            associate: function(models) {
-                Product.belongsTo(models.Category, {
-                    foreignKey: {
-                        //allowNull: false
-                    }
+            classMethods: {
+                associate: function(models) {
+                    Product.belongsTo(models.Category, {
+                        foreignKey: {
+                            //allowNull: false
+                        }
+                    });
                 }
             }
         }
-        }
-
-    });
+    );
 
     return Product;
-
 };
