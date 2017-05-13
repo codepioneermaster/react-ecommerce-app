8dc1a6f9f92f6ea6b8c26fde09e3818bd588b90a
supercodingfan <supercodingfan@gmail.com>
2017-05-13 13:33:54 -0500

    Update quan and delete item

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -26,6 +26,40 @@ function router(app) {
         response.send(err);
       });
   });
+
+  //update quantity
+   app.put("/cart/:userId/:itemId/:quantity", function(request, response) {
+    db.Cart
+      .update({quantity: request.params.quantity},{
+        where: {
+          UserId: request.params.userId,
+          ProductId: request.params.itemId
+        },
+        include: [db.Product]
+      })
+      .then(function(cartItems) {
+        response.json(cartItems);
+      })
+      .catch(function(err) {
+        console.log(err.message);
+        response.send(err);
+      });
+  });
+
+   //delete item from cart
+   app.delete('/cart/:userId/:itemId', function(request, response) {
+    db.Cart.destroy({
+      where: {
+        UserId: request.params.userId,
+        ProductId: request.params.itemId
+      }
+    }).then(function() {
+      response.redirect('/carts');
+    }).catch(function(err) {
+        console.log(err.message);
+        response.send(err);
+    });
+  });
 }
 
 module.exports = router;
