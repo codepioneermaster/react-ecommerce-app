95567beb525648743b0dab4c1df0dab59f235c86
supercodingfan <supercodingfan@gmail.com>
2017-05-12 23:56:36 -0500

    Start category controller

---
+++ controllers/categoriesController.js
@@ -0,0 +1,62 @@
+// Import User Model
+var db = require("../models");
+
+// ROUTES
+function router(app) {
+  // show all categories
+  app.get("/categories", function(request, response) {
+    db.Category.findAll({}).then(function(categories) {
+      response.json(categories);
+    });
+  });
+
+  // show products by category name
+  app.get("/category/:categoryName/products", function(request, response) {
+    db.Category
+      .findAll({
+        where: {
+          name: request.params.categoryName
+        },
+        include: [db.Product]
+      })
+      .then(function(categoryProducts) {
+        response.json(categoryProducts);
+      })
+      .catch(function(err) {
+        console.log(err.message);
+        response.send(err);
+      });
+  });
+
+  // show products by category id
+  app.get("/category/:id/products", function(request, response) {
+    db.Category
+      .findAll({
+        where: {
+          id: request.params.id
+        },
+        include: [db.Product]
+      })
+      .then(function(categoryProducts) {
+        response.json(categoryProducts);
+      })
+      .catch(function(err) {
+        console.log(err.message);
+        response.send(err);
+      });
+  });
+
+   // create new category 
+  app.post('/category', function(request, response) {
+    db.Category.create({
+      name: request.body.name
+    }).then(function(result) {
+      response.send('created ' + request.body.name);
+    }).catch(function(err) {
+        console.log(err.message);
+        response.send(err)
+    });
+  });
+}
+
+module.exports = router;
