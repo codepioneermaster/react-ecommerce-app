6efbac9662fae4882c7679eadf1d2e6e79adb8d2
supercodingfan <supercodingfan@gmail.com>
2017-05-13 10:49:43 -0500

    change category routes

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -11,7 +11,7 @@ function router(app) {
   });
 
   // show products by category name
-  app.get("/category/:categoryName/products", function(request, response) {
+  app.get("/category/name/:categoryName/products", function(request, response) {
     db.Category
       .findAll({
         where: {
@@ -29,7 +29,7 @@ function router(app) {
   });
 
   // show products by category id
-  app.get("/category/:id/products", function(request, response) {
+  app.get("/category/id/:id/products", function(request, response) {
     db.Category
       .findAll({
         where: {
