66ddc3b087e199bc69076e66e6accc09704bf8eb
supercodingfan <supercodingfan@gmail.com>
2017-05-14 17:59:05 -0500

    filtering products based on category

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -4,53 +4,38 @@ var db = require("../models");
 // ROUTES
 function router(app) {
   // show all categories
-  app.get("/categories", function(request, response) {
+  app.get("/categories", function(req, res) {
     db.Category.findAll({}).then(function(categories) {
-      response.json(categories);
+      res.json(categories);
     });
   });
 
   // show products by category name
-  app.get("/products/category/:categoryName", function(request, response) {
+  app.post("/products/category/:categoryName", function(req, res) {
     db.sequelize.Promise.all([
       db.Product.findAll({
         where: {
-          CategoryId: request.body.categoryId
-        }
+          CategoryId: req.body.categoryId
+       }
       }),
       db.Category.findAll({})
     ]).spread(function(products, categories) {
       res.render('products', {products, categories, user: req.user});
     });
-     
-    db.Category
-      .findAll({
-        where: {
-          name: request.params.categoryName
-        },
-        include: [db.Product]
-      })
-      .then(function(categoryProducts) {
-        response.json(categoryProducts);
-      })
-      .catch(function(err) {
-        console.log(err.message);
-        response.send(err);
-      });
   });
 
   // create new category
-  app.post("/category", function(request, response) {
+  app.post("/category", function(req, res) {
     db.Category
       .create({
-        name: request.body.name
+        name: req.body.name
       })
       .then(function(result) {
-        response.send("created " + request.body.name);
+        res.send("created " + req.body.name);
       })
       .catch(function(err) {
         console.log(err.message);
-        response.send(err);
+        res.send(err);
       });
   });
 }
