e3b882b885802c3d5cfc39439c7e3e9c82ee7f51
supercodingfan <supercodingfan@gmail.com>
2017-05-14 21:03:17 -0500

    update seed info

--- db/seeds.js
+++ db/seeds.js
@@ -2,95 +2,125 @@ var db = require("../models");
 
 module.exports = {
 	createSeedData: function() {
-		db.sequelize.sync({ force: true }).then(function() {
-			db.Category
-				.bulkCreate([
-					{
-						name: "Clothing"
-					},
-					{
-						name: "Shoes"
-					},
-					{
-						name: "Assesories"
-					}
-				])
-				.then(function() {
-					db.Product
-						.bulkCreate([
-							{
-								name: "Great Shirt",
-								price: 34.55,
-								img: "https://",
-								desc: "This shirt is faboulous",
-								quantity: 145,
-								CategoryId: 1
-							},
-							{
-								name: "Geezys",
-								price: 225.55,
-								img: "https://",
-								desc: "These Geezys are better than Yeezys",
-								quantity: 45,
-								CategoryId: 2
-							},
-							{
-								name: "Socks",
-								price: 22.99,
-								img: "https://",
-								desc: "Socks so good theyre socks",
-								quantity: 345,
-								CategoryId: 3
-							}
-						])
-						.then(function() {
-							db.User.bulkCreate([
+		// db.sequelize.dropAllTables
+			db.sequelize.sync({ force: true }).then(function() {
+				db.Category
+					.bulkCreate([
+						{
+							name: "Clothing"
+						},
+						{
+							name: "Shoes"
+						},
+						{
+							name: "Assesories"
+						}
+					])
+					.then(function() {
+						db.Product
+							.bulkCreate([
 								{
-									firstName: "Brad",
-									lastName: "Carlisle",
-									email: "brad@carlisle.com",
-									pwd: "$2a$10$SRA8F2QC.Vnsx5N97whEi.nmT1Rz88Bg8rZdTtcegp4WywT9RHT6y"
+									name: "Great Shirt",
+									price: 34.55,
+									img: "https://",
+									desc: "This shirt is faboulous",
+									quantity: 145,
+									CategoryId: 1
 								},
 								{
-									firstName: "Mike",
-									lastName: "Flight",
-									email: "mike@gmail.com",
-									pwd: "$2a$10$QohRiClPJgBWWZjb1KLNx.vwrjVRmnlipSOo9nWNfTs9FrfBpp69y"
+									name: "Geezys",
+									price: 225.55,
+									img: "https://",
+									desc: "These Geezys are better than Yeezys",
+									quantity: 45,
+									CategoryId: 2
 								},
 								{
-									firstName: "Jerry",
-									lastName: "Jepsen",
-									email: "jerry@jepsen.com",
-									pwd: "$2a$10$W2K0vNA0i24cROeHvaqDeu84UZAJ9EJ87q//xA9.phrhKOyvsKnO6"
+									name: "Socks",
+									price: 22.99,
+									img: "https://",
+									desc: "Socks so good theyre socks",
+									quantity: 345,
+									CategoryId: 3
 								}
-							]);
-						})
-						.then(function() {
-							db.Cart.bulkCreate([
-								{
-									UserId: 1,
-									ProductId: 1,
-									quantity: 2
-								},
-								{
-									UserId: 2,
-									ProductId: 3,
-									quantity: 1
-								},
-								{
-									UserId: 3,
-									ProductId: 1,
-									quantity: 1
+							])
+							.then(function() {
+								db.User.bulkCreate([
+									{
+										firstName: "Brad",
+										lastName: "Carlisle",
+										email: "brad@carlisle.com",
+										pwd: "$2a$10$SRA8F2QC.Vnsx5N97whEi.nmT1Rz88Bg8rZdTtcegp4WywT9RHT6y"
+									},
+									{
+										firstName: "Mike",
+										lastName: "Flight",
+										email: "mike@gmail.com",
+										pwd: "$2a$10$QohRiClPJgBWWZjb1KLNx.vwrjVRmnlipSOo9nWNfTs9FrfBpp69y"
+									},
+									{
+										firstName: "Jerry",
+										lastName: "Jepsen",
+										email: "jerry@jepsen.com",
+										pwd: "$2a$10$W2K0vNA0i24cROeHvaqDeu84UZAJ9EJ87q//xA9.phrhKOyvsKnO6"
+									}
+								]);
+							})
+							.then(function() {
+								db.Cart.bulkCreate([
+									{
+										UserId: 1,
+										ProductId: 1,
+										quantity: 2
+									},
+									{
+										UserId: 2,
+										ProductId: 3,
+										quantity: 1
+									},
+									{
+										UserId: 3,
+										ProductId: 1,
+										quantity: 1
 
-								},
-								{
-									UserId: 3,
-									ProductId: 2,
-									quantity: 4
-								},
-							]);
-						})
-				});
-		});
+									},
+									{
+										UserId: 3,
+										ProductId: 2,
+										quantity: 4
+									},
+								]);
+							}).then(function() {
+								db.Billing.bulkCreate([
+									{
+										billingName: "Fred Lewis",
+										billingAddress: "1735 N Wells St",
+										billingCity: "Chicago",
+										billingState: "IL",
+										billingZip: 60610,
+										billingCountry: "USA",
+										billingPhone: "7734904221",
+										OrderId: 1
+									},
+									
+								]);
+							}).then(function() {
+								db.Shipping.bulkCreate([
+									{
+										shippingName: "Fred Lewis",
+										shippingAddress: "1735 N Wells St",
+										shippingCity: "Chicago",
+										shippingState: "IL",
+										shippingZip: 60610,
+										shippingCountry: "USA",
+										shippingPhone: "7734904221",
+										OrderId: 1
+									}
+									
+								]);
+							})
+					});
+			});
+		
 	}
 };
